35ad400363189af518579bde3aac258f320c7544
Markprograme7 <i9marcosandre@gmail.com>
2024-11-29 21:51:15 -0300

    criado Javascrispts.js

---
+++ scripts.js
@@ -0,0 +1,35 @@
+function Acessibilidade() {
+    document.getElementById("login").style.backgroundImage='url("")';
+    document.getElementById("login").style.backgroundColor="green";
+    document.getElementById("div-login").style.width='44%';
+    document.getElementById("div-login").style.backgroundColor='yellow';
+    document.getElementById("label-usuario").style.fontSize="30px";
+    document.getElementById("label-senha").style.fontSize="30px";
+    document.getElementById("input-usuario").style.fontSize="30px";
+    document.getElementById("input-login").style.fontSize="30px";
+
+}
+
+function abrirdadospessoais(){
+    window.open("dadospessoais.html",'_self');
+}
+
+function abrirformacao(){
+    window.open("formacao.html",'_self');
+}
+
+function abrirexperiencias(){
+    window.open("experiencias.html",'_self');
+}
+
+function abrirlogin(){
+    window.open("login.html",'_self');
+}
+
+function abrirmenu(){
+    window.open("menu.html", '_self');
+}
+
+   
+    
+
