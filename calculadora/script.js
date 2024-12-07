dbd54e02355ef62a69cecfe11f8b84378389e529
Markprograme7 <i9marcosandre@gmail.com>
2024-12-06 21:28:12 -0300

    base da calculadora

---
+++ calculadora/script.js
@@ -0,0 +1,10 @@
+let v1, v2v total;
+
+function soma(){
+    v1=document.getElementById("valor 1").value;
+    v2=document.getElementById("valor 2").value;
+    total=parseInt(v1)+parseInt(v2);
+    document.getElementById("resultado").innerHTML="resultado"+total;
+
+
+}
